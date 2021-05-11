a82d03f45d75487958947a48853cbdbbfa8baf10
Станислав Болтенков <83593447+theProgrammerOfOurTime@users.noreply.github.com>
2021-05-12 00:24:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main()
 {
 	maze maz;
-	if (maz.input(10, 4, 4, "tests\\maze.txt")) { cout << maz.decid(0, 0, 0, 3, 9, 4) << endl; maz.print(); }
+	if (maz.input(10, 4, 4, "maze.txt")) { cout << maz.decid(0, 0, 0, 3, 9, 4) << endl; maz.print(); }
 	return 0;
 }
